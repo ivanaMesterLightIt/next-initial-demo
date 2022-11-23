cb52b2001efaccb4807cc8082d3ec28b734acfcb
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-11-23 15:53:39 -0300

    progress class 10 section 2

--- pages/about.js
+++ pages/about.js
@@ -1,23 +1,24 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Home - Ivana</title>
-        <meta name="description" content="Home Page" />
+        <title>About - Ivana</title>
+        <meta name="description" content="About Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ir a <a href="/about">About</a>
+          Ir a <a href="/">Home</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/about.js</code>
         </p>
       </main>
 
