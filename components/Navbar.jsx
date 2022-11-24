5bff4e6a5b89ed7ec4acab0f9b07c9c6368daefc
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-11-24 15:59:17 -0300

    progress class 17 section 2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,12 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
-
+import { ActiveLink } from "./ActiveLink";
+import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
     return (
-        <nav>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
+        <nav className={ styles['menu-container'] }>
+            <ActiveLink text="Home" href="/"/>
+            <ActiveLink text="About" href="/about"/>
+            <ActiveLink text="Contact" href="/contact"/>
         </nav>
     );
 }
